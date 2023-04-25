a7da5bf3ab64524270c3e2478d978c596b5a5412
ryo-kitatani <ryo0929m@gmail.com>
2023-04-26 00:10:33 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -89,3 +89,20 @@ const newNameArray = nameArry.map((name)=> {
 })
 
 console.log(newNameArray)
+
+/**
+ * 三項演算子
+ */
+const val1 = 1 > 0 ? "trueです" : "falseです"
+console.log(val1)
+
+const num = 1300;
+console.log(num.toLocaleString())
+const formattedNum = typeof num == 'number' ? num.toLocaleString() : "数値を入力してください。"
+console.log(formattedNum)
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "over 100" : "under 100"
+}
+
+console.log(checkSum(20, 90))
