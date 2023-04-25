6cc4b225acb3770da77dbf98443ea46d88b550cc
ryo-kitatani <ryo0929m@gmail.com>
2023-04-25 23:56:18 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -32,3 +32,26 @@ console.log(message4)
 const sayHello = (name = "guest") => console.log(`hello, ${name}`)
 sayHello('Kitatani')
 
+/**
+ * スプレッド構文
+ */
+const arry1 = [1,2];
+console.log(arry1)
+// 配列の中身を順序よく処理してくれる
+console.log(...arry1)
+
+const sum = (num1, num2) => console.log(num1 + num2)
+sum(arry1[0], arry1[1])
+sum(...arry1)
+
+const arry2 = [1,2,3,4,5];
+const [num1, num2, ...arry3] = arry2
+console.log(arry3)
+
+const arry4 = [10,20];
+const arry5 = [30, 40]
+const arry6 = [...arry4];
+console.log(arry6)
+
+const arry7 = [...arry4, ...arry5]
+console.log(arry7)
