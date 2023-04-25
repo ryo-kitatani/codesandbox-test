f94179ab29ba1977d2c1eee5051d6429a0b0fc82
ryo-kitatani <ryo0929m@gmail.com>
2023-04-26 00:49:40 +0900

    todo追加の実装

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // ulタグを作成
+  const ul = document.createElement("ul");
+  ul.className = "list-row";
+  // liタグの作成
+  const li = document.createElement("li")
+  li.innerHTML = inputText
+
+  ul.appendChild(li)
+  document.getElementById("incomplete-list").appendChild(ul)
+  console.log(ul)
+}
+
+document.getElementById("add-button").addEventListener("click", () => onClickAdd())
