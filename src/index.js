b30cef76f710a30c275a9dbe0c824c1a39d7f1c0
ryo-kitatani <ryo0929m@gmail.com>
2023-04-26 19:57:51 +0900

    完了するボタン機能追加

--- src/index.js
+++ src/index.js
@@ -9,25 +9,46 @@ const onClickAdd = () => {
   ul.className = "list-row";
   // liタグの作成
   const li = document.createElement("li")
-  li.innerHTML = inputText
+  li.innerText = inputText
 
   // button(完了)
   const completeBtn = document.createElement("button");
   completeBtn.innerText = "完了"
   completeBtn.addEventListener("click", () => {
-    console.log("完了")
+    deleteFromIncompleteList(completeBtn.parentNode);
+
+    const addTarget = completeBtn.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+    addTarget.textContent = null;
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    const backBtn = document.createElement("button");
+    backBtn.innerText = "戻す";
+
+    addTarget.appendChild(li)
+    addTarget.appendChild(backBtn)
+    console.log(addTarget)
+    document.getElementById("complete-list").appendChild(addTarget)
   })
 
   const deleteBtn = document.createElement("button");
   deleteBtn.innerText = "削除"
   deleteBtn.addEventListener("click", () => {
-    console.log("削除")
+    deleteFromIncompleteList(deleteBtn.parentNode);
   })
+
+
 
   ul.appendChild(li)
   ul.appendChild(completeBtn)
   ul.appendChild(deleteBtn)
   document.getElementById("incomplete-list").appendChild(ul)
+}
+
+// 未完リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target)
 }
 
 document.getElementById("add-button").addEventListener("click", () => onClickAdd())
