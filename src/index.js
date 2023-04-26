ba229b1ef0bfaaed71b72956f42f661e3142f47f
ryo-kitatani <ryo0929m@gmail.com>
2023-04-26 19:41:10 +0900

    buttonの追加

--- src/index.js
+++ src/index.js
@@ -11,9 +11,23 @@ const onClickAdd = () => {
   const li = document.createElement("li")
   li.innerHTML = inputText
 
+  // button(完了)
+  const completeBtn = document.createElement("button");
+  completeBtn.innerText = "完了"
+  completeBtn.addEventListener("click", () => {
+    console.log("完了")
+  })
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "削除"
+  deleteBtn.addEventListener("click", () => {
+    console.log("削除")
+  })
+
   ul.appendChild(li)
+  ul.appendChild(completeBtn)
+  ul.appendChild(deleteBtn)
   document.getElementById("incomplete-list").appendChild(ul)
-  console.log(ul)
 }
 
 document.getElementById("add-button").addEventListener("click", () => onClickAdd())
